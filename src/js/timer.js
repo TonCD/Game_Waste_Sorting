b7586f01ab6d1aa11070012ac9130830c441704b
toncd <92430972+TonCD@users.noreply.github.com>
2024-11-07 06:55:31 +0000

    update

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,50 +1,50 @@
-import { startGame, stopGame } from './game.js'
+import { startGame, stopGame } from "./game.js";
 
-const fullDasharray = 283
-const timeLimit = 20
-let timePassed = 0
-let timeLeft = timeLimit
-let timerInterval = null
-let isHiddenStartButton = false
+const fullDasharray = 283;
+const timeLimit = 30;
+let timePassed = 0;
+let timeLeft = timeLimit;
+let timerInterval = null;
+let isHiddenStartButton = false;
 
 export default function startTimer() {
   if (!isHiddenStartButton) {
-    const startButton = document.querySelector('.timer__start-button')
-    startButton.classList.add('hidden')
+    const startButton = document.querySelector(".timer__start-button");
+    startButton.classList.add("hidden");
   }
 
-  startGame()
+  startGame();
 
   timerInterval = setInterval(() => {
-    timePassed = timePassed += 1
-    timeLeft = timeLimit - timePassed
-    
-    setCircleDasharray()
+    timePassed = timePassed += 1;
+    timeLeft = timeLimit - timePassed;
+
+    setCircleDasharray();
 
     if (timeLeft === 0) {
-      onTimesUp()
-      stopGame()
-      timePassed = 0
-      timeLeft = timeLimit
-      setCircleDasharray()
+      onTimesUp();
+      stopGame();
+      timePassed = 0;
+      timeLeft = timeLimit;
+      setCircleDasharray();
     }
-  }, 1000)
+  }, 1000);
 }
 
 function onTimesUp() {
-  clearInterval(timerInterval)
+  clearInterval(timerInterval);
 }
 
 function setCircleDasharray() {
-  const circleDasharray = `${(
-    calculateTimeFraction() * fullDasharray
-  ).toFixed(0)} 283`
+  const circleDasharray = `${(calculateTimeFraction() * fullDasharray).toFixed(
+    0
+  )} 283`;
   document
-    .querySelector('.timer__path-remaining')
-    .setAttribute('stroke-dasharray', circleDasharray)
+    .querySelector(".timer__path-remaining")
+    .setAttribute("stroke-dasharray", circleDasharray);
 }
 
 function calculateTimeFraction() {
-  const rawTimeFraction = timeLeft / timeLimit
-  return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction)
+  const rawTimeFraction = timeLeft / timeLimit;
+  return rawTimeFraction - (1 / timeLimit) * (1 - rawTimeFraction);
 }
